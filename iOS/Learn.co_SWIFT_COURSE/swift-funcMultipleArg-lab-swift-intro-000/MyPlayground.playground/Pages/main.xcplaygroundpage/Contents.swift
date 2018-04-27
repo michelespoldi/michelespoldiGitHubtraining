450fface8fb819c6395d0b2f0170867a5dcc011c
michelespoldi <cykloppl@yahoo.co.uk>
2018-04-27 08:17:31 +0200

    multiple arguments function playground completed

---
+++ iOS/Learn.co_SWIFT_COURSE/swift-funcMultipleArg-lab-swift-intro-000/MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+/*: Outline
+ 
+ 
+ # Functions with multiple arguments
+ 
+ ### Readings associated with this lab
+ 
+ * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
+ * [Functions with multiple arguments](https://github.com/learn-co-curriculum/swift-funcMultipleArg-readme)
+
+ */
+
+/*: question1
+ ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
+ */
+func countriesPerContinent(continent: String, numberOfCountries: Int){
+    print("\(continent) is a continent which contains \(numberOfCountries) countries.")
+}
+
+
+/*: question2
+ ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
+ */
+func greeting(name: String, greeting: String) {
+    print("\(greeting), \(name)!")
+}
+
+greeting(name: "Danny", greeting: "Hello")
+
+
+
+
+/*: question3
+ ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
+ */
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) in \(month)")
+}
+
+daysInMonth(month: "November", days: 30)
+
+/*: question4
+ ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
+ */
+func alcoInventory(beverage: String, bottlesNumber: Int, storageLocation: String){
+    print("\(bottlesNumber) bottles of \(beverage) on the \(storageLocation)wall")
+}
+
+alcoInventory(beverage: "BlackLabel", bottlesNumber: 3, storageLocation: "Kitchen")
