ff8815db7f36cb6b67bf84505d478e4bc0c53b38
michelespoldi <cykloppl@yahoo.co.uk>
2018-04-29 08:30:04 +0200

    uiLab

---
+++ iOS/Learn.co_SWIFT_COURSE/Projects/projectUIbasics/projectUIbasics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectUIbasics
+//
+//  Created by Michele Spoldi on 29/04/2018.
+//  Copyright © 2018 Michele Spoldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
