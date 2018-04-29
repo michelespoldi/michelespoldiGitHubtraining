696e7e0ec86e6a6b16dd725129239ced758cdfb4
michelespoldi <cykloppl@yahoo.co.uk>
2018-04-29 13:16:10 +0200

    adding a short lesson of viewController lifecycle

---
+++ iOS/Learn.co_SWIFT_COURSE/Projects/swift-ViewLifeCycle-readme/ViewLifeCycle/Colors.swift
@@ -0,0 +1,16 @@
+//  Colors.swift
+
+
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func randomColor() -> UIColor {
+        return UIColor(red: .random(), green: .random(), blue: .random(), alpha: 1.0)
+    }
+}
